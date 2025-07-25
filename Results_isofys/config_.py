ffc664a06c40cfe33a5c067028c0ed33b3d2f5aa
ajchavez94 <125266441+ajchavez94@users.noreply.github.com>
2025-07-25 15:39:55 -0500

    Add files via upload

---
+++ Results_isofys/config_.py
@@ -0,0 +1,11 @@
+#standarize names
+#names sistematized
+named13C_cellulose = "δ13C (‰ v.s.V-PDB)"
+named13C_bulk = "bulk_δ13C (‰ v.s.V-PDB)"
+name18O = "δ18O in ‰ vs DeltaSMOW"
+named15N = "δ15N (‰ v.s. V-PDB)"
+nameC_bulk ="[C_b]%"
+nameC_cellulose = "[C]%"
+thick = "mean_thickness_(mm)"
+though = "mean_toughness_(kN/m)"
+LA = "Leaf area (cm²)"
